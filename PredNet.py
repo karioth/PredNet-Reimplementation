2ef82d20597cb8f58c88a2fb03364dfba8350ab0
karioth <149701938+karioth@users.noreply.github.com>
2024-04-02 20:49:44 +0200

    Update PredNet.py

--- PredNet.py
+++ PredNet.py
@@ -30,7 +30,7 @@ class PredNet_Cell(layers.Layer):
 
       #for r -- computes the representation used to make a prediction
         #self.convlstmcell = ConvLSTM2DCell(filters=self.R_stack_size, kernel_size=self.R_filt_size, padding='same', activation='relu', strides=(1, 1))
-        self.convlstmcell = ConvLSTM2DCell(filters=self.R_stack_size, kernel_size=self.R_filt_size, padding='same', activation='tahn', recurrent_activation='hard_sigmoid', strides=(1, 1))
+        self.convlstmcell = ConvLSTM2DCell(filters=self.R_stack_size, kernel_size=self.R_filt_size, padding='same', activation='tanh', recurrent_activation='hard_sigmoid', strides=(1, 1))
         self.upsample = layers.UpSampling2D(size=(2, 2))
 
       #for e -- computes the negative and possitive error.
