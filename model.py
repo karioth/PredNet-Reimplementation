928642f3850a6a3f6452a091a1b21ad07f3532a2
MarvinIRW <wurchmarvin@gmail.com>
2024-04-13 14:41:57 +0200

    added dogstrings

--- model.py
+++ model.py
@@ -6,7 +6,25 @@ from tensorflow.keras import models
 
 # PredNet model definition
 class PredNetModel(models.Model):
-    ''' Puts together a classical version of the PredNet architecture using the modern modular implementation'''
+    """
+    A Keras Model implementation of the PredNet architecture, adapted for flexible use in both training and inference with custom loss calculations.
+
+    Args:
+        stack_sizes (list): Sizes of the feature maps (channels) for each layer of the network.
+        R_stack_sizes (list): Sizes of the recurrent state of each layer (channels of the representation units).
+        A_filt_sizes (list): Filter sizes for the target computation layers.
+        Ahat_filt_sizes (list): Filter sizes for the prediction computation layers.
+        R_filt_sizes (list): Filter sizes for the representation computation layers.
+        layer_loss_weights (array): Weights for the loss at each layer, used for calculating weighted loss.
+        time_loss_weights (array): Weights over time steps for calculating the loss over time.
+        **kwargs: Additional keyword arguments for the TensorFlow Keras model.
+
+    Attributes:
+        prednet (PredNet): The core PredNet architecture encapsulated as a Keras RNN.
+        timeDense (TimeDistributed): A layer to apply layer loss weighting over time.
+        flatten (Flatten): Flattens the output to calculate a weighted loss.
+        dense (Dense): Computes a final scalar error to guide training.
+    """
     def __init__(self, stack_sizes, R_stack_sizes, A_filt_sizes, Ahat_filt_sizes, R_filt_sizes, layer_loss_weights, time_loss_weights,**kwargs):
         super(PredNetModel, self).__init__(**kwargs)
         
@@ -16,6 +34,7 @@ class PredNetModel(models.Model):
         self.Ahat_filt_sizes = Ahat_filt_sizes
         self.R_filt_sizes = R_filt_sizes
         
+        # Initialize PredNet cells based on provided configurations
         self.cells = [
                 PredNet_Cell(
                     stack_size=stack_size,
@@ -31,7 +50,7 @@ class PredNetModel(models.Model):
         self.layer_loss_weights = layer_loss_weights # weighting for each layer in final loss.
         self.time_loss_weights = time_loss_weights # weighting for the timesteps in final loss.
 
-        #PredNet architecture
+        # Set up the PredNet RNN architecture
         self.prednet = PredNet(cell = self.cells, return_sequences = True) # pass the cells to the PredNet(RNN) class
 
         #Layers for additional error computations for weighted loss during traning
@@ -47,8 +66,10 @@ class PredNetModel(models.Model):
 
     @tf.function
     def train_step(self, data):
-        #Override the train_step to make it work with both custom traning loops and model.fit() 
-        #Thanks to this, the PredNet can work on all modes seemlesly without having to define a new architecture when changing to inference/prediction mode
+        """
+        Overrides the default train_step to integrate custom loss calculation, so it works with custom training loops and model.fit().
+        Thanks to this, the PredNet can work on all modes seemlesly without having to define a new architecture when changing to inference/prediction mode.
+        """
         x, target = data
         with tf.GradientTape() as tape:
             all_error = self(x, training = True) #set traning = True to get errors as output
@@ -74,6 +95,9 @@ class PredNetModel(models.Model):
     
     @tf.function
     def test_step(self, data):
+        """
+        Custom test_step to evaluate the model during validation, using the same custom error computation as in training.
+        """
         # Similarly for the test_step. Traditionally traning would be set to false, but since here we use it to change the output to error, we set to true to evaluate loss performance.
         x_val, target_val = data
         all_error_val = self(x_val, training = True)
